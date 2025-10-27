7af2ccb64bcd7b5742ddbab093e33fb8e057c15c
Jistal <lleshi.xhibril@gmail.com>
2025-10-27 23:17:30 +0100

    set up admin dashboard controls

--- Booking System/src/Controllers/AdminController.java
+++ Booking System/src/Controllers/AdminController.java
@@ -1,9 +1,23 @@
 package Controllers;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 
 public class AdminController {
 
     private SceneManager sceneController;
+
+    @FXML private AnchorPane adminSideMenuContainer;
+    @FXML private Button adminManageCategoriesBtn;
+    @FXML private Button adminManageSlotsBtn;
+    @FXML private Button adminViewBookingsBtn;
+    @FXML private Button adminLogoutBtn;
+    @FXML private TextArea adminCategoriesTextArea;
+    @FXML private TextArea adminSlotsTextArea;
+    @FXML private TextField adminTextField;
 
     @FXML public void initiliaze(){
 
--- Booking System/src/Controllers/UserController.java
+++ Booking System/src/Controllers/UserController.java
@@ -1,4 +1,30 @@
 package Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class UserController {
+
+    @FXML private AnchorPane userRoot;
+    @FXML private AnchorPane userSideMenuContainer;
+    @FXML private Button bookAnAppointment;
+    @FXML private Button cancelAppointmentBtn;
+    @FXML private Button viewBookingsBtn;
+    @FXML private Button userLogoutBtn;
+
+    @FXML public void initialize(){
+
+
+        
+    }
+
+
+    private void setBookAnAppointment(){
+        bookAnAppointment.setOnAction(e ->{
+
+        });
+    }
+
+
 }
